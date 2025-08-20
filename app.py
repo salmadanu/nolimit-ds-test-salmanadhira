a86b1b95110d61819d6281b39495554e3e29ce38
salmadanu <salma.danuningrat@gmail.com>
2025-08-20 13:29:15 +0700

    Fixed vectorstore load type

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import streamlit as st
 import pickle
 from rag_utils import answer_query
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
 
 # ------------------- Load Vectorstore ------------------- #
 @st.cache_resource
 def load_vectorstore():
-    with open("vectorstore/index.pkl", "rb") as f:
-        return pickle.load(f)
-
-vectorstore = load_vectorstore()
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
+    return FAISS.load_local(
+        "faiss_index",
+        embedding_model,
+        allow_dangerous_deserialization=True
+    )
 
 # ------------------- UI ------------------- #
 st.title("RAG Chatbot on Computational Media Analysis")
