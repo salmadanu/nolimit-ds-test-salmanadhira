0217104617b796f7174bafd5864a042498b1a570
salmadanu <salma.danuningrat@gmail.com>
2025-08-20 15:30:34 +0700

    Back to flan-t5-base

--- rag_utils.py
+++ rag_utils.py
@@ -6,9 +6,10 @@ from langchain_huggingface import HuggingFaceEndpoint
 hf_token = st.secrets["api_keys"]["huggingface"]
 
 def answer_query(vectorstore, query):
-    llm = HuggingFaceEndpoint(repo_id ="meta-llama/Meta-Llama-3-8B-Instruct", 
-                               huggingfacehub_api_token=hf_token, 
-                              temperature= 0.6)
+    llm = HuggingFaceEndpoint(repo_id ="google/flan-t5-base", 
+                              huggingfacehub_api_token=hf_token, 
+                              temperature= 0.3,
+                              max_new_tokens=512)
     qa = RetrievalQA.from_chain_type(llm=llm, retriever=vectorstore.as_retriever())
     answer = qa({"query": query})
     return answer
